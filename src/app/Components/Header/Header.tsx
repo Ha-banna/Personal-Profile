1afcf7c0c0a9d26d90321ad66dd73d28ec89fca8
Ha-banna <hamzaalbanna5@gmail.com>
2024-01-17 21:29:32 +0300

    Adding items to home page

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -1,14 +1,19 @@
 import Nav from '../NavComponent/Nav';
 import './Header.css';
+import logo from '../../images/HA.png'
+import { Link } from 'react-router-dom';
 
 export default function Header (){
     return(
         <>
             <header>
-                <Nav to='/' totxt='Home'></Nav>
-                <Nav to='/projects' totxt='Projects'></Nav>
-                <Nav to='/aboutme' totxt='About Me'></Nav>
-                
+                <Link to='/'><img src={logo} className='logo-img'></img></Link>
+                <div className='nav-cont'>
+                    <Nav to='/' totxt='Home'></Nav>
+                    <Nav to='/projects' totxt='Projects'></Nav>
+                    <Nav to='/aboutme' totxt='About Me'></Nav>
+                </div>
+                <div></div>
             </header>
             <div className="hr"></div>
         </>
---
+++ src/app/Components/ProfilePic/PFP.tsx
@@ -0,0 +1,10 @@
+import './PFP.css'
+import profile_pic from '../../images/profile-picture.jpeg'
+
+export default function PFP(){
+    return (
+        <div className='pfp-cont'>
+            <img src={profile_pic} alt="profile picture" className="profile-pic" />
+        </div>
+    );
+}
