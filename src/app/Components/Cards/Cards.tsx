c99b6ef008987f33ff5472b9930f55d2eada3ae6
unknown <hamzaalbanna5@gmail.com>
2024-11-11 21:55:01 +0300

    fixing bugs

--- src/app/Components/Cards/Cards.tsx
+++ src/app/Components/Cards/Cards.tsx
@@ -2,7 +2,7 @@ import './Cards.css'
 //<img src={props.img} alt='project' className='card-image'/>
 export default function Cards(props: {project_name: string, link: string}){
     return(
-        <a href={props.link}>
+        <a href={props.link} target='_blank'>
             <div className='card'>
                 <h3 className='project-name'>{props.project_name}</h3>
             </div>
